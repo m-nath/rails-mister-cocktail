2605c1d7febf42baacfc1f233a512c5e5296de60
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-09 16:03:49 +0900

    add image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_08_031939) do
+ActiveRecord::Schema.define(version: 2019_08_09_042738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_08_08_031939) do
     t.string "dose"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
