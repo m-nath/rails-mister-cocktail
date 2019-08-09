622c82ca76a16beb81c5e88e2a4943b157150e88
nath mongkolrathmanee <m.nath1105@gmail.com>
2019-08-09 12:31:08 +0900

    remove JSON from seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Ingredient.destroy_all
-require 'JSON'
-require 'open-uri'
+# require 'JSON'
+# require 'open-uri'
 
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 json = open(url).read
